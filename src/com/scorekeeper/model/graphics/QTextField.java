7408ba2578f0ad8f4a63f4112ee44328ff4c8ec3
ConwayJ18 <conway.jess18@gmail.com>
2019-04-07 19:28:45 -0500

    Refactored QTextField switch, added color

--- src/com/scorekeeper/model/graphics/QTextField.java
+++ src/com/scorekeeper/model/graphics/QTextField.java
@@ -1,6 +1,7 @@
 package com.scorekeeper.model.graphics;
 
 import java.awt.Button;
+import java.awt.Color;
 import java.awt.Font;
 
 import javax.swing.JTextField;
@@ -116,34 +117,34 @@ public class QTextField extends JTextField
 	private void format()
 	{
 		int type = this.type;
+		Color lightGray = new Color(243,243,243); //Light gray
+		this.setBackground(lightGray);
+		this.setHorizontalAlignment(QTextField.CENTER);
+        this.setBorder(null);
+        this.setVisible(true);
+		
 		switch(type)
 		{
 			case 1: //Timekeeper Display Parts
-				Font fieldFont = new Font("MarkerFelt-Wide", Font.PLAIN, 126);
-				this.setFont(fieldFont);
+				Font timeFont = new Font("MarkerFelt-Wide", Font.PLAIN, 126);
+				this.setFont(timeFont);
 		        this.setEditable(false);
-		        this.setHorizontalAlignment(QTextField.CENTER);
-		        this.setBorder(null);
 	            break; 
 	        case 2: //Score Display
 	        	Font scorebox = new Font("MarkerFelt-Wide", Font.PLAIN, 256); //Create a font
 	    		this.setEditable(false); //You cannot type into scoreboxes
-	    		this.setHorizontalAlignment(JTextField.CENTER); //Text in scoreboxes should be centered
 	    		this.setFont(scorebox); //Scoreboxes should use the font created above
 	            break; 
 			case 3: //Time Input
 				Font inputTimeFont = new Font("MarkerFelt-Wide", Font.PLAIN, 48); //Create font for inputTime
-				this.setHorizontalAlignment(JTextField.CENTER); //inputTime text should be centered
 				this.setFont(inputTimeFont); //inputTime should have the font created above
 	            break; 
 	        case 4: //Score Input
 	        	Font inputScoreFont = new Font("MarkerFelt-Wide", Font.PLAIN, 72); //Create font for setScore1
-	    		this.setHorizontalAlignment(JTextField.CENTER); //setScore1 text should be centered
 	    		this.setFont(inputScoreFont); //setScore1 should have the font created above
 	            break; 
 	        case 5: //Notification Display
 	        	Font notificationFont = new Font("MarkerFelt-Wide", Font.PLAIN, 24); //Create font for inputTime
-	    		this.setHorizontalAlignment(JTextField.CENTER); //inputTime text should be centered
 	    		this.setFont(notificationFont); //inputTime should have the font created above
 	    		this.setEditable(false); //Notification field should not be editable
 	            break; 
