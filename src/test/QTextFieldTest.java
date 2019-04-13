4afa075221dab12c7f54542203c9db75b3d8521d
Cong <cwang20@luc.edu>
2019-04-12 23:16:34 -0500

    Refactoring QTestFieldTest

--- src/test/QTextFieldTest.java
+++ src/test/QTextFieldTest.java
@@ -9,21 +9,94 @@ import javax.swing.text.PlainDocument;
 import java.awt.*;
 
 class QTextFieldTest {
-	QTextField TX = new QTextField();
+
+	QTextField A = new QTextField();
+	QTextField B = new QTextField("text");
+	QTextField C = new QTextField(1);
+	QTextField D = new QTextField("text", 2);
+	QTextField E = new QTextField("text", 5, 5, 5, 5);
+	QTextField F = new QTextField(3, 5, 5, 5, 5);
+	QTextField H = new QTextField("text", 4, 5, 5, 5, 5);
 
 	@Test
-	void type1_test() {
-		QTextField H = new QTextField("text", 1, 5, 5, 5, 5);
+	void A_test() {
+		assertEquals("", A.getText());
+
+	}
+
+	@Test
+	void B_test() {
+		assertEquals("text", B.getText());
+
+	}
+
+	@Test
+	void C_test() {
 		Color type1_background = new Color(243, 243, 243);
-		assertEquals(type1_background, H.getBackground());     //test background color setting for type1
-		assertEquals(QTextField.CENTER, H.getHorizontalAlignment());     //test horizontal alignment setting for type1
+		assertEquals(type1_background, C.getBackground());     //test background color setting for type1
+		assertEquals(QTextField.CENTER, C.getHorizontalAlignment());     //test horizontal alignment setting for type1
+		assertNull(C.getBorder());     //test border setting
+		assertTrue(C.isVisible());     //test visibility setting
+		assertEquals("Dialog", C.getFont().getFamily());     //test font family
+		assertEquals("MarkerFelt-Wide", C.getFont().getName());     //test font name
+		assertEquals(Font.PLAIN, C.getFont().getStyle());     //test font style
+		assertEquals(126, C.getFont().getSize());     //test font size
+	}
+
+	@Test
+	void D_test() {
+		Color type2_background = new Color(243, 243, 243);
+		assertEquals(type2_background, D.getBackground());     //test background color setting for type2
+		assertEquals(QTextField.CENTER, D.getHorizontalAlignment());     //test horizontal alignment setting for type2
+		assertNull(D.getBorder());     //test border setting
+		assertTrue(D.isVisible());     //test visibility setting
+		assertEquals("text", D.getText());     //test text field setting
+		assertEquals("Dialog", D.getFont().getFamily());     //test font family
+		assertEquals("MarkerFelt-Wide", D.getFont().getName());     //test font name
+		assertEquals(Font.PLAIN, D.getFont().getStyle());     //test font style
+		assertEquals(256, D.getFont().getSize());     //test font size
+	}
+
+	@Test
+	void E_test() {
+		assertEquals("text", E.getText());     //test text field setting
+		assertEquals(5, E.getX());
+		assertEquals(5, E.getY());
+		assertEquals(5, E.getWidth());
+		assertEquals(5, E.getHeight());     //test position and dimension of QTextField
+	}
+
+	@Test
+	void F_test() {
+		Color type3_background = new Color(243, 243, 243);
+		assertEquals(type3_background, F.getBackground());     //test background color setting for type3
+		assertEquals(QTextField.CENTER, F.getHorizontalAlignment());     //test horizontal alignment setting for type3
+		assertNull(F.getBorder());     //test border setting
+		assertTrue(F.isVisible());     //test visibility setting
+		assertEquals("Dialog", F.getFont().getFamily());     //test font family
+		assertEquals("MarkerFelt-Wide", F.getFont().getName());     //test font name
+		assertEquals(Font.PLAIN, F.getFont().getStyle());     //test font style
+		assertEquals(48, F.getFont().getSize());     //test font size
+		assertEquals(5, F.getX());
+		assertEquals(5, F.getY());
+		assertEquals(5, F.getWidth());
+		assertEquals(5, F.getHeight());     //test position and dimension of QTextField
+	}
+
+
+	@Test
+	void H_test() {
+
+		Color type4_background = new Color(243, 243, 243);
+		assertEquals(type4_background, H.getBackground());     //test background color setting for type4
+		assertEquals(QTextField.CENTER, H.getHorizontalAlignment());     //test horizontal alignment setting for type4
 		assertNull(H.getBorder());     //test border setting
 		assertTrue(H.isVisible());     //test visibility setting
 		assertEquals("text", H.getText());     //test text field setting
-		assertEquals("Dialog",  H.getFont().getFamily());     //test font family
+		assertEquals("Dialog", H.getFont().getFamily());     //test font family
 		assertEquals("MarkerFelt-Wide", H.getFont().getName());     //test font name
 		assertEquals(Font.PLAIN, H.getFont().getStyle());     //test font style
-		assertEquals(126, H.getFont().getSize());     //test font size
+		assertEquals(72, H.getFont().getSize());     //test font size
 		assertEquals(5, H.getX());
 		assertEquals(5, H.getY());
 		assertEquals(5, H.getWidth());
@@ -32,60 +105,19 @@ class QTextFieldTest {
 	}
 
 	@Test
-	void type2_test() {
-		QTextField I = new QTextField("text", 2, 5, 5, 5, 5);
-		Color type2_background = new Color(243, 243, 243);
-		assertEquals(type2_background, I.getBackground());     //test background color setting for type2
-		assertEquals(QTextField.CENTER, I.getHorizontalAlignment());     //test horizontal alignment setting for type2
-		assertNull(I.getBorder());     //test border setting
-		assertTrue(I.isVisible());     //test visibility setting
-		assertEquals("text", I.getText());     //test text field setting
-		assertEquals("Dialog",  I.getFont().getFamily());     //test font family
-		assertEquals("MarkerFelt-Wide", I.getFont().getName());     //test font name
-		assertEquals(Font.PLAIN, I.getFont().getStyle());     //test font style
-		assertEquals(256, I.getFont().getSize());     //test font size
-		assertEquals(5, I.getX());
-		assertEquals(5, I.getY());
-		assertEquals(5, I.getWidth());
-		assertEquals(5, I.getHeight());     //test position and dimension of QTextField
-	}
+	void setBounds_test() {
+		A.setBounds(5,5,5,5,5);
+		assertEquals(5, A.getX());
+		assertEquals(5, A.getY());
+		assertEquals(5, A.getWidth());
+		assertEquals(5, A.getHeight());
+		assertEquals(5, A.getFont().getSize());
+		E.setBounds(10,10,10,10);
+		assertEquals(10, E.getX());
+		assertEquals(10, E.getY());
+		assertEquals(10, E.getWidth());
+		assertEquals(10, E.getHeight());
 
-	@Test
-	void type3_test() {
-		QTextField J = new QTextField("text", 3, 5, 5, 5, 5);
-		Color type3_background = new Color(243, 243, 243);
-		assertEquals(type3_background, J.getBackground());     //test background color setting for type3
-		assertEquals(QTextField.CENTER, J.getHorizontalAlignment());     //test horizontal alignment setting for type3
-		assertNull(J.getBorder());     //test border setting
-		assertTrue(J.isVisible());      //test visibility setting
-		assertEquals("text", J.getText());     //test text field setting
-		assertEquals("Dialog",  J.getFont().getFamily());     //test font family
-		assertEquals("MarkerFelt-Wide", J.getFont().getName());     //test font name
-		assertEquals(Font.PLAIN, J.getFont().getStyle());     //test font style
-		assertEquals(48, J.getFont().getSize());     //test font size
-		assertEquals(5, J.getX());
-		assertEquals(5, J.getY());
-		assertEquals(5, J.getWidth());
-		assertEquals(5, J.getHeight());     //test position and dimension of QTextField
-	}
-
-	@Test
-	void type4_test() {
-		QTextField K = new QTextField("text", 4, 5, 5, 5, 5);
-		Color type4_background = new Color(243, 243, 243);
-		assertEquals(type4_background, K.getBackground());     //test background color setting for type4
-		assertEquals(QTextField.CENTER, K.getHorizontalAlignment());     //test horizontal alignment setting for type4
-		assertNull(K.getBorder());     //test border setting
-		assertTrue(K.isVisible());     //test visibility setting
-		assertEquals("text", K.getText());     //test text field setting
-		assertEquals("Dialog",  K.getFont().getFamily());     //test font family
-		assertEquals("MarkerFelt-Wide", K.getFont().getName());     //test font name
-		assertEquals(Font.PLAIN, K.getFont().getStyle());     //test font style
-		assertEquals(72, K.getFont().getSize());     //test font size
-		assertEquals(5, K.getX());
-		assertEquals(5, K.getY());
-		assertEquals(5, K.getWidth());
-		assertEquals(5, K.getHeight());     //test position and dimension of QTextField
 	}
 
 	@Test
@@ -106,4 +138,7 @@ class QTextFieldTest {
 		assertEquals(5, L.getWidth());
 		assertEquals(5, L.getHeight());     //test position and dimension of QTextField
 	}
+}
+
+
 }
